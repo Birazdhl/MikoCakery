27968889087364d315ce0ae75d12849ebe594984
birazdhl <birazdhl@gmail.com>
2020-07-21 02:05:10 +0545

    Product Image

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import HeaderMenu from '../../component/Header/HeaderMenu';
+import HeaderMenu from '../../component/Footer/HeaderMenu';
 import Footer from '../../component/Header/Footer';
-import { Container } from 'semantic-ui-react';
+import { Container, Segment } from 'semantic-ui-react';
+import Product from '../../component/Products/Product';
 
 function App() {
   return (
     <div className="App">
       <Container fluid>
         <HeaderMenu />
-
+        <Product />
         <Footer />
       </Container>
 
---
+++ src/component/Products/Product.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Container } from 'semantic-ui-react';
+import { Container as BootstrapContainer, Card, Row, Col } from "react-bootstrap";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Slider from "react-slick";
+
+const numberOfPicture = [1, 2, 3, 4, 5, 6];
+
+const Product = () => {
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    };
+    return (
+        <Container>
+            <div className="header-wrapper">
+                <header id="productHeader">
+                    <h1>Designer Cakes Delivered to Your Doorstep
+                </h1>
+                    <h2><b>All cakes are tailored to your taste and style</b></h2>
+                </header>
+            </div>
+            <Slider {...settings} >
+                {numberOfPicture.map((picture, index) => (
+                    <div key={index} id="productLionel">
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img
+                                    variant="top"
+                                    src={`assets/cakes/cake-${picture}.jpg`}
+                                />
+                            </Card>
+                        </Col>
+                    </div>
+
+                ))}
+            </Slider>
+        </Container>
+
+
+
+    );
+}
+
+export default Product
+
